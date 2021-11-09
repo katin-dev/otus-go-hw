59a0fb5bfedc20d050175fdeaa1a5c366b030a2f
katinsv <s.katin@tinkoff.ru>
2021-11-10 00:19:04 +0300

    HW07 Просто написал программу, которая копирует файлы

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"flag"
+	"io"
+	"math"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -18,5 +23,66 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	// 1. Открыть файл, если могу на чтение
+	// 2. Открыть файл, если могу, на запись
+	// 3. Проверить, что offset не выходит за размер файла
+	// 4. Оказывается, у файла может быть неизвестная длина! Тогда fatal!
+	// 5. Скопировать файл и отметить progress
+
+	if from == "" {
+		panic("-from must not be empty")
+	}
+	if to == "" {
+		panic("-to must not be empty")
+	}
+
+	source, err := os.Open(from)
+	if err != nil {
+		panic("failed to open source file: " + err.Error())
+	}
+	defer source.Close()
+
+	sourceInfo, err := source.Stat()
+	if err != nil {
+		panic("failed to stat source file: " + err.Error())
+	}
+
+	if offset != 0 && offset > sourceInfo.Size() {
+		panic("offset is out of bound:")
+	}
+
+	dest, err := os.Create(to)
+	if err != nil {
+		panic("Failed to open dest file")
+	}
+	defer dest.Close()
+
+	source.Seek(offset, io.SeekStart)
+
+	start := offset
+	stop := sourceInfo.Size()
+	if limit != 0 {
+		stop = int64(math.Min(float64(start+limit), float64(sourceInfo.Size())))
+	}
+
+	bytesToCopy := stop - start
+
+	var bufferSize int = 4
+	var writtenBytes int64
+
+	// Рассчитаем progress bar - сколько буферов потребуется для копирования файла
+	iterations := int(math.Ceil(float64(bytesToCopy) / float64(bufferSize)))
+
+	bar := pb.StartNew(iterations)
+	for writtenBytes < bytesToCopy {
+		n := int64(math.Min(float64(bufferSize), float64(bytesToCopy-writtenBytes)))
+		io.CopyN(dest, source, n)
+
+		writtenBytes += n
+
+		bar.Increment()
+	}
+
+	bar.Finish()
 }
