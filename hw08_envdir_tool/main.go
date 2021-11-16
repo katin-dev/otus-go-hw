b78f83ca4bdfb254ff951a5a9e636fffa04aeeb1
katin-dev <katin.dev@gmail.com>
2021-11-16 21:19:06 +0300

    HW08 Запуск программы в main.go

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -3,22 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
 	args := os.Args
-	if len(args) < 2 {
+	if len(args) < 3 {
 		usage()
 	}
 
 	path := args[1]
+	command := args[2]
 
 	env, err := ReadDir(path)
 	if err != nil {
 		panic("Failed to read " + path + ": " + err.Error())
 	}
 
-	fmt.Println(env)
+	// Запустить программу
+	// Связать stdin, stdout, stderr -> программа
+
+	// make map from vars
+	varsMap := make(map[string]string)
+	for _, v := range os.Environ() {
+		parts := strings.Split(v, "=")
+		varsMap[parts[0]] = strings.Join(parts[1:], "=")
+	}
+
+	// Process vars map
+	for varName := range env {
+		if env[varName].NeedRemove {
+			delete(varsMap, varName)
+		} else {
+			varsMap[varName] = env[varName].Value
+		}
+	}
+
+	// Convert map to []strings
+	execVars := make([]string, 0)
+	for varName := range varsMap {
+		execVars = append(execVars, varName+"="+varsMap[varName])
+	}
+
+	cmd := exec.Command(command, args[3:]...)
+	cmd.Env = execVars
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
 
 func usage() {
