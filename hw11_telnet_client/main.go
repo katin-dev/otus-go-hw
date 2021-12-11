8368d0bd9d8f2a78029afb4c772751eb5403cb03
Sergey <katin.dev@gmail.com>
2021-12-11 14:59:32 +0300

    HW11 telnet client (#13)
    * HW11 Telnet Client
    
    * HW11 CR #1
    
    Co-authored-by: katinsv <s.katin@tinkoff.ru>

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,64 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	flag "github.com/spf13/pflag"
+)
+
 func main() {
+	timeout := flag.Duration("timeout", 0, "connection timeout")
+	flag.Parse()
+
+	host := strings.TrimSpace(flag.Arg(0))
+	if len(host) == 0 {
+		usage()
+	}
+
+	port := strings.TrimSpace(flag.Arg(1))
+	if len(port) == 0 {
+		usage()
+	}
+
+	t := NewTelnetClient(host+":"+port, *timeout, os.Stdin, os.Stdout)
+
+	if err := t.Connect(); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to connect to server: ", err)
+		os.Exit(1)
+	}
+
+	ctx, ctxCancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	go func() {
+		if err := t.Send(); err != nil {
+			t.Close()
+			ctxCancel()
+			log.Printf("stdin closed: %s\n", err)
+			return
+		}
+	}()
+
+	go func() {
+		if err := t.Receive(); err != nil {
+			os.Stdin.Close()
+			ctxCancel()
+			log.Printf("connection colsed: %s\n", err)
+			return
+		}
+	}()
+
+	<-ctx.Done()
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+}
+
+func usage() {
+	fmt.Println("Usage:\n\tgo-telnet [--timeout=10] host port")
+	os.Exit(0)
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +15,56 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &SimpleTelnet{
+		address, timeout, in, out, nil,
+	}
+}
+
+// P.S. Author's solution takes no more than 50 lines.
+
+type SimpleTelnet struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	con     net.Conn
+}
+
+func (c *SimpleTelnet) Connect() error {
+	con, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+
+	c.con = con
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *SimpleTelnet) Send() error {
+	if c.con == nil {
+		return fmt.Errorf("connection is not established")
+	}
+
+	_, err := io.Copy(c.con, c.in)
+
+	return err
+}
+
+func (c *SimpleTelnet) Receive() error {
+	if c.con == nil {
+		return fmt.Errorf("connection is not established")
+	}
+
+	_, err := io.Copy(c.out, c.con)
+
+	return err
+}
+
+func (c *SimpleTelnet) Close() error {
+	if c.con != nil {
+		return c.con.Close()
+	}
+
+	return nil
+}
