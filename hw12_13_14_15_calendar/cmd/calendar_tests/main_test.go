55af837b056f6da188de47145df57aecb57d3828
katinsv <s.katin@tinkoff.ru>
2022-01-23 00:28:36 +0300

    HW15 Integration tests

---
+++ hw12_13_14_15_calendar/cmd/calendar_tests/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(t *testing.T) {
+	/*
+		1. Дождаться когда сервис поднимится
+		2. CRUDL для HTTP
+		3. CRUDL для gRPC
+		4. Хз как проверить нотификацию
+	*/
+
+	require.True(t, true)
+}
